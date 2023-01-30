74c58a7a8d53066d7f2f898d0c4a48555cf3c5f5
Fedde van Gils <feddevangils@gmail.com>
2023-01-30 14:05:25 +0100

    add same layout to my pins

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -1,6 +1,16 @@
 <x-guest-layout>
-     <div class="m-4 columns-2 md:columns-3 lg:columns-4">
+    <div class="md:hidden">
         <x-search-filter />
+    </div>
+    @if(count($pins) > 1)
+     <div class="m-4 columns-2 md:columns-3 lg:columns-4">
+    @else
+     <div class="m-4 flex w-2/3 items-start gap-4">
+    @endif
+        <div class="hidden md:block">
+            <x-search-filter />
+        </div>
+
         @foreach($pins as $pin)
             @livewire('pins.card', ['pin' => $pin], key($pin->id . '-card'))
         @endforeach
