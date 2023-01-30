c2082f83e2a4e6aa22a883a11b13abf854434ec8
Fedde van Gils <feddevangils@gmail.com>
2023-01-30 11:18:17 +0100

    add dashboard link

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -19,6 +19,13 @@
                         <a href="{{route('pin.create')}}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             + Nieuw
                         </a>
+                        @auth
+                            @if(\Auth::user()->hasRole('admin'))
+                            <a href="{{route('dashboard')}}" class="inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                <b> Dashboard </b>
+                            </a>
+                            @endif
+                        @endauth
                         {{-- search field --}}
 
                     </div>
